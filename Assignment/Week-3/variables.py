be13178fa8aeefb6fe18859e4d415bd97ed816ca
Ogbogo Jackson <ckjzone2000@gmail.com>
2024-10-30 05:50:45 +0100

    Basic Python Syntax

---
+++ Assignment/Week-3/variables.py
@@ -0,0 +1,11 @@
+My_Age = 24 
+
+My_Height = 1.75 
+
+My_Name = 'Jackson'
+
+print(My_Age) # This is a variable that prints my name to the console.
+
+print(My_Height) # This is a variable that prints my height in centimetre to the console
+
+print(My_Name) # This is a variable that prints my name to the console.
